1087d27c96fc3e487d3ba025bad6547fcaeb1e95
Damián Silvani <munshkr@gmail.com>
2019-06-15 12:18:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 const withCSS = require('@zeit/next-css');
 
+const assetPrefix = process.env.NODE_ENV === 'production' ? '/learn-pianotype' : '';
+
 module.exports = withCSS({
-  // some configuration
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/learn-pianotype' : '',
-  // another configuration
+  assetPrefix,
+  publicRuntimeConfig: {
+    assetPrefix
+  },
+  webpack: function(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]"
+        }
+      }
+    });
+    return config;
+  }
 });
